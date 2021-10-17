339cbe8c28ac6a4c7c0f3dad6c6ecbf5601dbe29
chokoladis <igor2011__igor@mail.ru>
2021-10-17 19:23:02 +0400

    2.5 hours in work

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        dots:true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000
+      });
+});
